14ac3aa2fbf37d4e8ee8d5967682411cdaaf672b
LoveHeart <113758022+LeeyooMuzic@users.noreply.github.com>
2024-05-03 20:48:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/AboutSilentBoY",
+    "https://t.me/DeluxNetwork",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
